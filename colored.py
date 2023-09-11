480c001922a8e8086fb5a019e5a91d23ed282d06
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-09-11 14:00:32 -0700

    Create colored.py

---
+++ colored.py
@@ -0,0 +1,20 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    YELLOW = "\033[0;33m"
+    WHITE = "\033[0;37m"
+    NOCOLOR = "\033[0m"
+    LIGHT_RED = "\033[1;31m"
+    LIGHT_CYAN = "\033[1;36m"
+
+class decorations:
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
