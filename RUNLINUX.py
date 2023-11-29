139733e3769790167f817e3be1810fcd7c970d96
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-11-29 23:58:14 +0200

    Update RUNLINUX.py

--- RUNLINUX.py
+++ RUNLINUX.py
@@ -3,15 +3,13 @@ from threading import Thread
 
 truncFiles = ["Files/newestProxy.txt", "Files/validPr.txt"]
 
-print()
-
 def deleteFileData(e):
     with open(f"{e}", "a") as f : f.truncate(0)
 
 for j in truncFiles : Thread(target = deleteFileData(j)).start()
 
-def file1():  subprocess.call("cmd /c scraping.py")
-def file2():  subprocess.call("cmd /c validateProxy.py")
+def file1():  subprocess.call("python scraping.py", shell = True)
+def file2():  subprocess.call("python validateProxy.py", shell = True)
 
 Thread(target = file1).start()
 Thread(target = file2).start()
