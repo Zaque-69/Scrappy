b17160d90ac58c8f5eb209b1d579f4838b4e08ac
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-12-13 21:34:06 +0200

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,4 +1,4 @@
-import os, re, shutil, requests,  webbrowser, customtkinter, random, json
+import os, re, shutil, requests,  webbrowser, customtkinter, random, subprocess, json
 
 from tkinter import *
 from threading import Thread
@@ -20,56 +20,59 @@ class scrap:
         Label(master, bg="#363047", text="Web Scraping", font=(14), fg="white").place(x = 150, y = 30)
         Label(master, text="Please insert your link:", bg="#363047", fg="white").place(x = 50, y = 70)
         Label(master, bg="#363047", text = "").pack()
-        Label(master, text="Status Code :", bg="#363047", fg="white").place(x = 725, y = 155)
+        Label(master, text="Status Code :", bg="#363047", fg="white").place(x = 665, y = 155)
         Label(master, bg="#363047", text="© 2023 Z4que ALL RIGHTS RESERVED", fg="white").place(x=20, y=390)
-        Label(master, bg="#363047", text="URLs Found", fg="white", font=(14)).place(x=530, y = 30)
+        Label(master, bg="#363047", text="URLs Found", fg="white", font=(14)).place(x=500, y = 30)
         Label(master, bg="#363047", text="Hidden Pages", fg="white", font=(14)).place(x=140, y = 230)
-        Label(master, bg="#363047", text="Proxy Info", fg="white", font=(14)).place(x=540, y = 230)
-        Label(master, bg="#363047", text="E-mails Found", fg="white", font=(14)).place(x=775, y=30)
-        Label(master, bg="#363047", text="User Agent", fg="white", font=(14)).place(x=790, y=230)
+        Label(master, bg="#363047", text="Proxy Info", fg="white", font=(14)).place(x=510, y = 230)
+        Label(master, bg="#363047", text="E-mails Found", fg="white", font=(14)).place(x=715, y=30)
+        Label(master, bg="#363047", text="User Agent", fg="white", font=(14)).place(x=700, y=230)
+        Label(master, bg="#363047", text="Payload", fg="white", font=(14)).place(x=950, y=30)
 
         #entries 
-        self.inputtxt = customtkinter.CTkEntry(master, placeholder_text="https://www.example.com/", width=300, height=25, border_width=2, corner_radius=5, fg_color = "#0E0C12", text_color = "white",border_color="#000")
-        self.name = customtkinter.CTkEntry(master, placeholder_text="Name (optional)", width=300, height=25, border_width=2, corner_radius=5, fg_color = "#0E0C12", text_color = "white", border_color="#000")
-        self.search = customtkinter.CTkEntry(master, placeholder_text="Search", width=300, height=25, border_width=2, corner_radius=5, fg_color = "#0E0C12", text_color = "white",border_color="#000")
-        self.st = customtkinter.CTkEntry(master, placeholder_text="XXX", width=130, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#80C148", border_color="#0E0C12")
-        self.totalFiles = customtkinter.CTkEntry(master, width=220, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#968BC4", border_color="#0E0C12") 
+        self.inputtxt = customtkinter.CTkEntry(master, placeholder_text="https://www.example.com/", width=270, height=25, border_width=2, corner_radius=5, fg_color = "#0E0C12", text_color = "white",border_color="#000")
+        self.name = customtkinter.CTkEntry(master, placeholder_text="Name (optional)", width=270, height=25, border_width=2, corner_radius=5, fg_color = "#0E0C12", text_color = "white", border_color="#000")
+        self.search = customtkinter.CTkEntry(master, placeholder_text="Search", width=270, height=25, border_width=2, corner_radius=5, fg_color = "#0E0C12", text_color = "white",border_color="#000")
+        self.st = customtkinter.CTkEntry(master, placeholder_text="XXX", width=100, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#80C148", border_color="#0E0C12")
+        self.totalFiles = customtkinter.CTkEntry(master, width=190, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#968BC4", border_color="#0E0C12") 
 
-        self.choseYourIp = customtkinter.CTkEntry(master, placeholder_text="255.0.0.0:PORT", width=160, height=25, border_width=2, corner_radius=5, fg_color = "#0E0C12", text_color = "#D69044", border_color="#000")
-        self.currentIp = customtkinter.CTkEntry(master, placeholder_text="Your IP", width=220, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#D69044", border_color="#0E0C12")
-        self.changedIp = customtkinter.CTkEntry(master, placeholder_text="Changed IP", width=220, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#D69044", border_color="#0E0C12") 
-
+        self.choseYourIp = customtkinter.CTkEntry(master, placeholder_text="255.0.0.0:PORT", width=130, height=25, border_width=2, corner_radius=5, fg_color = "#0E0C12", text_color = "#D69044", border_color="#000")
+        self.currentIp = customtkinter.CTkEntry(master, placeholder_text="Your IP", width=190, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#D69044", border_color="#0E0C12")
+        self.changedIp = customtkinter.CTkEntry(master, placeholder_text="Changed IP", width=190, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#D69044", border_color="#0E0C12") 
 
         #findHidden
         self.inputtxt.place(x = 50, y = 100)
         self.name.place(x = 50, y = 140)
         self.search.place(x = 50, y = 180)
-        self.st.place(x = 815, y = 155)
-        self.totalFiles.place(x = 475, y = 180) 
+        self.st.place(x = 755, y = 155)
+        self.totalFiles.place(x = 445, y = 180) 
 
-        self.choseYourIp.place(x = 475, y = 267)
-        self.changedIp.place(x = 475, y = 307)
-        self.currentIp.place(x = 475, y = 347) 
+        self.choseYourIp.place(x = 445, y = 267)
+        self.changedIp.place(x = 445, y = 307)
+        self.currentIp.place(x = 445, y = 347) 
 
         #buttosn
-        customtkinter.CTkButton(master, width=60, height=60, text="Run", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.main).place(x = 380, y = 100)
-        customtkinter.CTkButton(master, width=60, height=40, text="Find", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.threadFindHidden).place(x = 380, y = 270)
-        customtkinter.CTkButton(master, width=60, height=40, text="Reload", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.reload).place(x = 380, y = 330)
-        customtkinter.CTkButton(master, width=60, height=30, text="Search", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.searchLink).place(x = 380, y = 177.5)
-        customtkinter.CTkButton(master, width=30, height=30, text="Servers", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.openservers).place(x = 645, y = 263.5)
+        customtkinter.CTkButton(master, width=60, height=60, text="Run", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.main).place(x = 350, y = 100)
+        customtkinter.CTkButton(master, width=60, height=40, text="Find", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.threadFindHidden).place(x = 350, y = 270)
+        customtkinter.CTkButton(master, width=60, height=40, text="Reload", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.reload).place(x = 350, y = 330)
+        customtkinter.CTkButton(master, width=60, height=30, text="Search", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.searchLink).place(x = 350, y = 177.5)
+        customtkinter.CTkButton(master, width=30, height=30, text="Servers", fg_color="#574E73", hover_color="#706494", text_color="black", border_width=1, corner_radius=5, border_color="#4B4363", command = self.openservers).place(x = 585, y = 263.5)
 
         #text areas
-        self.output = customtkinter.CTkTextbox(master , width=300, height=100, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#8D8B93", border_color="#0E0C12")
-        self.domains = customtkinter.CTkTextbox(master, width=220, height=80, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#E7CF50", border_color="#0E0C12")
-        self.afis = customtkinter.CTkTextbox(master , width=220, height=100, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#D0669F", border_color="#0E0C12")
-        self.yourUserAgent = customtkinter.CTkEntry(master, placeholder_text="Your User Agent (optional)", width=220, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#D0669F", border_color="#0E0C12")
-        self.userAgent = customtkinter.CTkTextbox(master, width=220, height=70, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#74CFC1", border_color="#0E0C12")
+        self.output = customtkinter.CTkTextbox(master , width=270, height=100, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#8D8B93", border_color="#0E0C12")
+        self.domains = customtkinter.CTkTextbox(master, width=190, height=80, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#E7CF50", border_color="#0E0C12")
+        self.afis = customtkinter.CTkTextbox(master , width=190, height=100, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#D0669F", border_color="#0E0C12")
+        self.yourUserAgent = customtkinter.CTkEntry(master, placeholder_text="Your User Agent (optional)", width=190, height=25, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#D0669F", border_color="#0E0C12")
+        self.userAgent = customtkinter.CTkTextbox(master, width=190, height=70, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#74CFC1", border_color="#0E0C12")
+        self.payload = customtkinter.CTkTextbox(master, width=190, height = 300, border_width=2, corner_radius=5, fg_color = "#191621", text_color = "#74CFC1", border_color="#0E0C12")
+
 
         self.output.place(x = 50, y = 270)
-        self.afis.place(x = 475, y = 70)
-        self.domains.place(x = 725, y = 70)
-        self.yourUserAgent.place(x = 725, y = 265)
-        self.userAgent.place(x = 725, y = 300)
+        self.afis.place(x = 445, y = 70)
+        self.domains.place(x = 665, y = 70)
+        self.yourUserAgent.place(x = 665, y = 265)
+        self.userAgent.place(x = 665, y = 300)
+        self.payload.place(x = 890, y = 70)
 
         self.phtotExtensions = ["jpeg", "jpg", "png", "gif", 
                                "tiff", "psd", "pdf", "eps", "ai"]
@@ -80,7 +83,9 @@ class scrap:
         with open("Files/hidden_adr.txt", "w") as nush: pass
 
     def openservers(self) : 
-        os.system('Files\ValidPr.txt')
+        print(os.getcwd())
+        try : subprocess.call('Files\ValidPr.txt')
+        except : subprocess.call('cat File\ValidPr.txt', shell = True)
 
     def searchLink(self):
         SEARCH = self.search.get()
@@ -150,14 +155,29 @@ class scrap:
             try:
                 randomValidProxy = self.choseYourIp.get()
 
-                gt = requests.get(str(INPUT), headers = {
+                if ( len(self.payload.get("0.0", "end")) == 1) : 
+
+                    gt = requests.post(str(INPUT), headers = {
+                        "User-Agent" : randomValue 
+                            },
+                            proxies = {
+                                "http" : randomValidProxy, 
+                                "https" : randomValidProxy
+                            }
+                        )
+                
+                else :
+                    gt = requests.post(str(INPUT), headers = {
                     "User-Agent" : randomValue 
                         },
                         proxies = {
                             "http" : randomValidProxy, 
                             "https" : randomValidProxy
-                        }
+                        }, 
+                        payload = self.payload.get('0.0', 'end')
                     )
+
+                
                 print(bcolors.HEADER + 'EXIT STATUS CODE : ' + decorations.BOLD + bcolors.LIGHT_RED + f'{gt.raise_for_status} ' + decorations.ENDC)
                 sp = BeautifulSoup(gt.content, "html.parser")
 
@@ -285,10 +305,9 @@ class scrap:
     def threadFindHidden(self): Thread(target = self.findHidden).start()
 
 root = Tk()
-root.geometry("975x420")
+root.geometry("1110x420")
 root.configure(bg="#363047")
 root.config(cursor = "top_left_arrow")
-root.wm_attributes('-toolwindow', 'True')
 root.resizable(False, False)
 root.title("Web Scraping")
 
