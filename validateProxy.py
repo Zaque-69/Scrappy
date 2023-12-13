11e5c7478b89ad89288e3aea1b702599ac4b6431
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-12-13 21:36:12 +0200

    Update validateProxy.py

--- validateProxy.py
+++ validateProxy.py
@@ -2,7 +2,8 @@ import requests, threading, queue, subprocess, sys
 
 q = queue.Queue()
 
-subprocess.call("cmd /c getProxy.py")
+try : subprocess.call("cmd /c getProxy.py")
+except : subprocess.call("python3 getProxy.py", shell = True)
 
 with open("Files/newestProxy.txt", "r") as f : 
     proxies = f.read().split("\n")
@@ -17,7 +18,7 @@ def chechProxies():
     
     while not q.empty():
         proxy = q.get()
-        #print("start")
+
         try:
             res = requests.get("https://ipinfo.io/json", 
                 proxies = {
@@ -25,21 +26,11 @@ def chechProxies():
                 }
             )
         except : continue
+
         if res.status_code == 200:
-
             with open("Files/validPr.txt", "a") as newFile : 
-                newFile.write(str(proxy))
-                newFile.write("\n")
-            
+                newFile.write(str(proxy) + '\n')
             count += 1
-
         if count == 3 : return 
- 
         
 for _ in range(1000) : threading.Thread( target = chechProxies ).start()
-
-#print("FILE 2 FINISHED")
-#C:\Users\zamfi\OneDrive\Desktop
-
-#https://ipinfo.io/json
-#https://jsonip.com/
